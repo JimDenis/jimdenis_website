c74c357778b08a5fd7c35e007de91f7757021131
Jim Denis <jimdenis007@yahoo.com>
2020-07-23 12:12:20 -0700

    Updates

---
+++ precache-manifest.b1b65a8914f3616a32fa718b1329ffe1.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c75df60e5b8b848518e23ba84c5806",
+    "url": "/jim_website/index.html"
+  },
+  {
+    "revision": "4f51283696529a87bc41",
+    "url": "/jim_website/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "128d23f9c6e4f79b162b",
+    "url": "/jim_website/static/css/main.fdc64ff4.chunk.css"
+  },
+  {
+    "revision": "4f51283696529a87bc41",
+    "url": "/jim_website/static/js/2.92854869.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jim_website/static/js/2.92854869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128d23f9c6e4f79b162b",
+    "url": "/jim_website/static/js/main.cb836346.chunk.js"
+  },
+  {
+    "revision": "7fc07ee0d272de35651c",
+    "url": "/jim_website/static/js/runtime-main.feea3d75.js"
+  },
+  {
+    "revision": "c619f3847728aef3930f38cb560d0b03",
+    "url": "/jim_website/static/media/CSS2.c619f384.png"
+  },
+  {
+    "revision": "24ef6aaf5db268ad43c249855c6ec91a",
+    "url": "/jim_website/static/media/DB2.24ef6aaf.jpg"
+  },
+  {
+    "revision": "8b67d1064dc6b182de524fd187b0331f",
+    "url": "/jim_website/static/media/ExpressJS.8b67d106.png"
+  },
+  {
+    "revision": "fc876044f33dce61561c8b2e79cc7e5f",
+    "url": "/jim_website/static/media/Firebase.fc876044.jpg"
+  },
+  {
+    "revision": "64cb402f63a30fd63511fd2401bf055f",
+    "url": "/jim_website/static/media/MyPic.64cb402f.jpeg"
+  },
+  {
+    "revision": "9085fda022653b12ea582fea6dfabcd2",
+    "url": "/jim_website/static/media/Resume.9085fda0.docx"
+  },
+  {
+    "revision": "924041bae5660c7121442b7f29801c17",
+    "url": "/jim_website/static/media/Styled_Components.924041ba.png"
+  },
+  {
+    "revision": "cfecb2da14ecb652a50758fd12f2d5cb",
+    "url": "/jim_website/static/media/bootstrap_logo.cfecb2da.png"
+  },
+  {
+    "revision": "b04355ec49d64c2eaa0880c2fd88ed7f",
+    "url": "/jim_website/static/media/day_planner.b04355ec.png"
+  },
+  {
+    "revision": "e0e9ac58f05417cd5d812da9ad9e47a3",
+    "url": "/jim_website/static/media/football-pool.e0e9ac58.png"
+  },
+  {
+    "revision": "1b56ac823b007a5a8d72da99ff4ee008",
+    "url": "/jim_website/static/media/html-logo.1b56ac82.jpg"
+  },
+  {
+    "revision": "12bac607c472dfee8483d057e8515ec2",
+    "url": "/jim_website/static/media/javascript-logo.12bac607.png"
+  },
+  {
+    "revision": "75e950f08c9bc63e10a8d6c1bf671814",
+    "url": "/jim_website/static/media/kardasian_group_project.75e950f0.png"
+  },
+  {
+    "revision": "4dae2a92d1ed3bef91314a67474f2606",
+    "url": "/jim_website/static/media/mongodb-logo.4dae2a92.jpg"
+  },
+  {
+    "revision": "eb4bc9eba97b27ca9949690bcfc74194",
+    "url": "/jim_website/static/media/password_gen.eb4bc9eb.png"
+  },
+  {
+    "revision": "e53ca18496e38cc97021285c81d8cd05",
+    "url": "/jim_website/static/media/project_vibes.e53ca184.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jim_website/static/media/react-logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8df85537a28b53385f6e887c798380bd",
+    "url": "/jim_website/static/media/weather_forecaster.8df85537.png"
+  }
+]);
